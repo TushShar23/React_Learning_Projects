063ed1a58d5d2fcc3009546fa9fb39909cca2651
Tushar Sharma <onlyforcode23@gmail.com>
2025-11-29 12:08:29 +0000

    complete maincontent compnonent and whole project

---
+++ Project-1(Landing Page)/Components/MainContent.jsx
@@ -0,0 +1,30 @@
+import '../src/App.css'
+
+function MainContent(){
+    return(
+        <div className="main">
+            <div className="leftbox">
+                <h1>your feet deserve the best</h1>
+                <div className="ptbn">
+                    <p>your feet deserve the best and we're here to help you with our shoes.your feet deserve the best and we're here to help you with our shoes.</p>
+                    <div className="btns">
+                        <button id='btn1'>Shop Now</button>
+                        <button id='btn2'>Category</button>
+                    </div>
+                </div>
+                <div className="sellers">
+                    <p>Also Available On:</p>
+                    <img src="../src/assets/flipkart.png" alt="logo"  style={{height:"50px"}}/>
+                    <img src="../src/assets/amazon.png" alt="logo"  style={{height:"50px"}}/>
+                </div>
+
+            </div>
+            <div className="rightbox">
+                <img src="../src/assets/heroimg.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+
+export default MainContent
--- Project-1(Landing Page)/src/App.jsx
+++ Project-1(Landing Page)/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from '../Components/Navbar'
+import MainContent from '../Components/MainContent'
 
 function App() {
   return(
-    <div>
-      <Navbar></Navbar>
+    <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
+      <Navbar />
+      <MainContent />
     </div>
   )
 }
