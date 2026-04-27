f4db88723759f3c5a7d7dbfd917b6aa11d0b7c70
Tushar Sharma <onlyforcode23@gmail.com>
2026-04-27 12:53:04 +0100

    started new weather app project, done with high level design of app

---
+++ Project-2(Weather App)/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import Weather_Card from '../src/Weather_Card.jsx'
+
+function App() {
+  return(
+    <Weather_Card />
+  )
+}
+
+export default App
---
+++ Project-2(Weather App)/src/Weather_Card.jsx
@@ -0,0 +1,11 @@
+import "../src/Card.css"
+const Weather_Card = ()=> {
+    
+  return (
+    <div className="main_cont">
+        Hello World
+    </div>
+  )
+}
+
+export default Weather_Card
