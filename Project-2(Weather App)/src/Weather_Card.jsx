7470f9d65859f16b3009ae50fb3dae1fbe74ed4f
Tushar Sharma <onlyforcode23@gmail.com>
2026-04-29 10:37:57 +0100

    improve structure,design,clean UI and complete the project

--- Project-2(Weather App)/src/Weather_Card.jsx
+++ Project-2(Weather App)/src/Weather_Card.jsx
@@ -10,6 +10,17 @@ const Weather_Card = ()=> {
     const [data,setData] = useState(null)
     const [city,setCity] = useState("")
     const [searchCity,setSearchCity] = useState("")
+
+    const currDate = new Date();
+    const monthLists = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    // this is for binding the month number and month name together
+
+    const day = currDate.getDate()
+    const month = monthLists[currDate.getMonth()]
+    const year = currDate.getFullYear()
+
+    const formattedData = `${month} ${day},${year}`
+
     
     // searchcity will contain the actual city name which we wanted to search else TOO MANY API CALLS will happen which is not a good thing(if we type D then API CALL -> De then API CALL -> Del then API CALL and so on....)
 
@@ -39,16 +50,20 @@ const Weather_Card = ()=> {
 
     return (
         <div className="main_cont">
+            <h1>WEATHER APP</h1>
             {data && 
-            <div>
-              {data.name}
-              {data.main.temp}
-              {data.weather[0].main}
+            <div className="main_data">
+              <p className="DateInfo">{formattedData}</p>  
+              <p className="DateInfo">{data.name}</p>
               <img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="Weather icon" />
+              <p className="tempData">{data.main.temp}</p>
+              <p className="tempData">{data.weather[0].main}</p>
             </div>
             }
-            <input type="text" name="myCity" id="mycity" value={city} onChange={(e)=>{setCity(e.target.value)}}/>
-            <button onClick={()=>{setSearchCity(city)}}>Get</button>
+            <div className="inputsearch">
+                <input type="text" name="myCity" id="mycity" value={city} onChange={(e)=>{setCity(e.target.value)}}/>
+                <button onClick={()=>{setSearchCity(city)}}>Get</button>
+            </div>
         </div>
     )
 }
